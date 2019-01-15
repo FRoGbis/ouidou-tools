9b9893342591ad1baa015af2c9fa34f050c1e462
franckbebel <franck.bebel@ouidou.fr>
2019-01-15 15:54:10 +0100

    Updates

---
+++ precache-manifest.9f956291be63e6c8a7586cbae01a5ada.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f01f9b0a7d044b5b414638d667e533c9",
+    "url": "/ouidou-tools/static/media/formationJira.f01f9b0a.jpg"
+  },
+  {
+    "revision": "0c56adfc70f001d86f67",
+    "url": "/ouidou-tools/static/css/main.938b4a4e.chunk.css"
+  },
+  {
+    "revision": "4dbf0f3c8ee02bf943e8",
+    "url": "/ouidou-tools/static/js/1.4dbf0f3c.chunk.js"
+  },
+  {
+    "revision": "a692306c713595a0c680",
+    "url": "/ouidou-tools/static/js/runtime~main.a692306c.js"
+  },
+  {
+    "revision": "14fdc748108d838a40b71ca9de11695c",
+    "url": "/ouidou-tools/static/media/confluence.14fdc748.jpg"
+  },
+  {
+    "revision": "a6f348e57871db27563f4ee0542d68ef",
+    "url": "/ouidou-tools/static/media/support.a6f348e5.jpg"
+  },
+  {
+    "revision": "2d85a42bb2f84950a2f76c9bfd903fd3",
+    "url": "/ouidou-tools/static/media/demoJira.2d85a42b.jpg"
+  },
+  {
+    "revision": "bb8806f2bc65339d02d242f9cee9419c",
+    "url": "/ouidou-tools/static/media/formationConfluence.bb8806f2.jpg"
+  },
+  {
+    "revision": "0c56adfc70f001d86f67",
+    "url": "/ouidou-tools/static/js/main.0c56adfc.chunk.js"
+  },
+  {
+    "revision": "63dda348452a74661544685950fe3bb2",
+    "url": "/ouidou-tools/static/media/fisheye.63dda348.jpg"
+  },
+  {
+    "revision": "4df22bf9f32a85f90c0a8cfb21619359",
+    "url": "/ouidou-tools/static/media/jenkins.4df22bf9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ouidou-tools/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "743b70401463b69c2ad39cac6682b3e1",
+    "url": "/ouidou-tools/static/media/bamboo.743b7040.jpg"
+  },
+  {
+    "revision": "1cdfbc6ce8c623b87f3652b97a15a3b7",
+    "url": "/ouidou-tools/static/media/bitbucket.1cdfbc6c.jpg"
+  },
+  {
+    "revision": "993e8982f858bbeeeb198ff785604b13",
+    "url": "/ouidou-tools/static/media/hipchat.993e8982.jpg"
+  },
+  {
+    "revision": "261f25155b80a6eeb6429f6758036dc1",
+    "url": "/ouidou-tools/static/media/nexus.261f2515.jpg"
+  },
+  {
+    "revision": "ed898758fa10e85e74ece02f4d60419c",
+    "url": "/ouidou-tools/index.html"
+  }
+];
